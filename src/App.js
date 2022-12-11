babc080434d4dbb18c6b153fff18f2a494128ac8
eduardo-3ct <eduardo@3ct.mx>
2022-12-11 13:38:57 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { DropdownItem, DropdownMenu, DropdownToggle, Nav, Navbar, NavbarBrand, UncontrolledDropdown } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBeer } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+  const brand = 'We Love Beer';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar color='light'>
+        <NavbarBrand href="/">
+          <FontAwesomeIcon icon={faBeer} className='me-2 text-warning' />
+          {brand}
+        </NavbarBrand>
+        <Nav>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>My account</DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem>Gift Table</DropdownItem>
+              <DropdownItem>Wish List</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Create a list</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
